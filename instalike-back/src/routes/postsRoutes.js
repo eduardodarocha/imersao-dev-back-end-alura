a0ebff0708fc55e5ae05eb6ba7880a06a3a81b7e
Eduardo Rocha <eduardomfrocha@gmail.com>
2024-11-23 22:11:24 -0300

    Added build script

--- instalike-back/src/routes/postsRoutes.js
+++ instalike-back/src/routes/postsRoutes.js
@@ -4,9 +4,13 @@ import cors from "cors";
 import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
 
 const corsOptions = {
-  origin: "http://localhost:8000",
+  origin: "*",
   optionsSuccessStatus: 200
 }
+// const corsOptions = {
+//   origin: "http://localhost:8000",
+//   optionsSuccessStatus: 200
+// }
 
 
 const storage = multer.diskStorage({
